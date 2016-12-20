95b8bc321993bfbfb92b5a0858e7593aada97377
Ալեքսանդր Առաքելյան <bambrikii@gmail.com>
2016-12-20 20:36:02 +0100

    - bubble sort.

--- src/main/java/org/bambrikii/examples/sorting/bubble/BubbleSort.java
+++ src/main/java/org/bambrikii/examples/sorting/bubble/BubbleSort.java
@@ -7,12 +7,12 @@ public class BubbleSort implements Sortable {
 
 	public int[] sort(int[] array) {
 		int t;
-		for (int i = 0; i < array.length - 1; i++) {
-			for (int j = i + 1; j < array.length; j++) {
-				if (array[i] > array[j]) {
-					t = array[i];
-					array[i] = array[j];
-					array[j] = t;
+		for (int i = array.length - 1; i > 0; i--) {
+			for (int j = 0; j < i; j++) {
+				if (array[j] > array[j + 1]) {
+					t = array[j];
+					array[j] = array[j + 1];
+					array[j + 1] = t;
 					ArrayAsStringFactory.log(array);
 				}
 			}
