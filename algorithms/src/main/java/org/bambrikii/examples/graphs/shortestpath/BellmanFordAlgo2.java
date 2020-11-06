5fe9c26c0700b58e11d28cb7b400fec9fdecd880
Ալեքսանդր Արաքելյան <bambrikii@gmail.com>
2020-11-05 19:09:44 -0500

    - Bellman-Ford algorithm - Shortest Path.

--- algorithms/src/main/java/org/bambrikii/examples/graphs/shortestpath/BellmanFordAlgo2.java
+++ algorithms/src/main/java/org/bambrikii/examples/graphs/shortestpath/BellmanFordAlgo2.java
@@ -2,63 +2,74 @@ package org.bambrikii.examples.graphs.shortestpath;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
 /**
+ * https://www.youtube.com/watch?v=lyw4FaxrwHg&ab_channel=WilliamFiset
  * https://www.youtube.com/watch?v=obWXjtg0L64&t=249s&ab_channel=MichaelSambol
  */
 public class BellmanFordAlgo2 {
-    private Map<Integer, List<BellmanFordEdge2>> allEdges = new HashMap<>();
+    private final Map<Integer, List<BellmanFordEdge2>> allEdges = new HashMap<>();
 
     public BellmanFordAlgo2 edge(int from, int to, int weight) {
         if (!this.allEdges.containsKey(from)) {
             this.allEdges.put(from, new ArrayList<>());
+        }
+        if (!this.allEdges.containsKey(to)) {
+            this.allEdges.put(to, new ArrayList<>());
         }
         this.allEdges.get(from).add(new BellmanFordEdge2(from, to, weight));
         return this;
     }
 
     public Map<Integer, BellmanFordEdge2> find(int from) {
-        Map<Integer, BellmanFordEdge2> weights = new LinkedHashMap<>();
+        Map<Integer, BellmanFordEdge2> currEdges = new HashMap<>();
+        for (Integer edge : allEdges.keySet()) {
+            currEdges.put(edge, new BellmanFordEdge2(null, edge, Integer.MAX_VALUE));
+        }
+        currEdges.get(from).setWeight(0);
 
-        LinkedList<Integer> q = new LinkedList<>();
-        q.addLast(from);
-
-        while (!q.isEmpty()) {
-            Integer fromVertex = q.poll();
-            List<BellmanFordEdge2> edges = allEdges.get(fromVertex);
-            if (edges == null) {
-                continue;
-            }
-            BellmanFordEdge2 fromWeight = weights.get(fromVertex);
-            for (BellmanFordEdge2 edge : edges) {
-                Integer to = edge.getTo();
-                Integer w = edge.getWeight();
-
-                if (to == null) {
-                    continue;
+        for (Map.Entry<Integer, List<BellmanFordEdge2>> entry : allEdges.entrySet()) {
+            Integer fromVertex = entry.getKey();
+            List<BellmanFordEdge2> edges = entry.getValue();
+            BellmanFordEdge2 fromCurrEdge = currEdges.get(fromVertex);
+            for (BellmanFordEdge2 fromEdge : edges) {
+                Integer toVertex = fromEdge.getTo();
+                BellmanFordEdge2 toCurrEdge = currEdges.get(toVertex);
+                int expectedWeight = fromCurrEdge.getWeight() + fromEdge.getWeight();
+                if (expectedWeight < toCurrEdge.getWeight()) {
+                    toCurrEdge.setFrom(fromVertex);
+                    toCurrEdge.setWeight(expectedWeight);
                 }
-                BellmanFordEdge2 toW = weights.get(to);
-                if (fromWeight == null) {
-                    weights.put(to, new BellmanFordEdge2(fromVertex, to, w));
-                } else if (toW == null || toW.getWeight() > fromWeight.getWeight() + w) {
-                    weights.put(to, new BellmanFordEdge2(fromVertex, to, fromWeight.getWeight() + w));
-                }
-                q.addLast(to);
             }
         }
 
-        return weights;
+
+        for (Map.Entry<Integer, List<BellmanFordEdge2>> entry : allEdges.entrySet()) {
+            Integer fromVertex = entry.getKey();
+            List<BellmanFordEdge2> edges = entry.getValue();
+            BellmanFordEdge2 fromCurrEdge = currEdges.get(fromVertex);
+            for (BellmanFordEdge2 fromEdge : edges) {
+                Integer toVertex = fromEdge.getTo();
+                BellmanFordEdge2 toCurrEdge = currEdges.get(toVertex);
+                int expectedWeight = fromCurrEdge.getWeight() + fromEdge.getWeight();
+                if (toCurrEdge.getWeight() < expectedWeight) {
+//                    toCurrEdge.setFrom(fromVertex);
+                    toCurrEdge.setWeight(Integer.MIN_VALUE);
+                }
+            }
+        }
+
+        return currEdges;
     }
 
     public void print(Map<Integer, BellmanFordEdge2> result) {
         Integer weight = null;
         BellmanFordEdge2 edge = result.get(6);
+        System.out.println("Path:");
         while (edge != null) {
-            System.out.println(edge);
+            System.out.println(" " + edge);
             if (weight == null) {
                 weight = edge.getWeight();
             } else {
@@ -68,5 +79,10 @@ public class BellmanFordAlgo2 {
         }
 
         System.out.println("Weight: " + weight);
+        System.out.println("All entries:");
+
+        for (Map.Entry<Integer, BellmanFordEdge2> entry : result.entrySet()) {
+            System.out.println(" " + entry.getKey() + " -> " + entry.getValue());
+        }
     }
 }
