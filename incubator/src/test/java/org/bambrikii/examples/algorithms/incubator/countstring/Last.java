79e4e21d5ca08af3bf9b6ff61661aa4cb9a22fcd
Ալեքսանդր Արաքելյան <bambrikii@gmail.com>
2021-02-15 13:05:38 -0500

    - Algo tasks removed .

--- incubator/src/test/java/org/bambrikii/examples/algorithms/incubator/countstring/Last.java
+++
@@ -1,8 +0,0 @@
-package org.bambrikii.examples.algorithms.incubator.countstring;
-
-public class Last extends Node {
-    @Override
-    boolean match(Ctx ctx) {
-        return ctx.remaining() == 0;
-    }
-}
