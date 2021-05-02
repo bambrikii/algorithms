5c8efeb64c71ff1634b38b4461e661c66dea568c
Ալեքսանդր Արաքելյան <bambrikii@gmail.com>
2021-05-02 11:58:46 -0400

    - Union-Find fixed

--- algorithms/src/main/java/org/bambrikii/examples/graphs/unionfind/UnionFind.java
+++ algorithms/src/main/java/org/bambrikii/examples/graphs/unionfind/UnionFind.java
@@ -12,18 +12,31 @@ public class UnionFind {
     }
 
     public UnionFind join(int left, int right) {
-        values.put(right, left);
+        values.put(left, right);
         return this;
     }
 
     public boolean find(int left, int right) {
-        return values.get(left) == values.get(right);
+        Integer leftVal = values.get(left);
+        if (leftVal.equals(right)) {
+            return true;
+        }
+        if (leftVal.equals(left)) {
+            return leftVal.equals(right);
+        }
+        boolean found = find(leftVal, right);
+        if (found) {
+            values.put(left, right);
+        }
+        return found;
     }
 
     public static void main(String[] args) {
         UnionFind algo = new UnionFind();
-        algo.add(1).add(2).add(3).join(1, 2);
-        System.out.println(algo.find(1, 2)); // true
-        System.out.println(algo.find(1, 3)); // false
+        algo.add(1).add(2).add(3).add(4).add(5)
+                .join(1, 2).join(2, 4).join(4, 5);
+        System.out.println("1 -> 5 should be true: " + algo.find(1, 5));
+        System.out.println("1 -> 3 should be false: " + algo.find(1, 3));
+        System.out.println("1 -> 5 should be fast and true: " + algo.find(1, 5));
     }
 }
