7abbdf0d0cfd69a4ecc6d808d2f7d50b20ed620a
Ալեքսանդր Արաքելյան <bambrikii@gmail.com>
2021-05-14 08:41:51 -0400

    - Factorial cached.

--- algorithms/src/main/java/org/bambrikii/examples/combinatorics/factorial/FactorialMain.java
+++ algorithms/src/main/java/org/bambrikii/examples/combinatorics/factorial/FactorialMain.java
@@ -1,17 +1,45 @@
 package org.bambrikii.examples.combinatorics.factorial;
 
 public class FactorialMain {
-    public static void main(String[] args) {
-        FactorialMain main = new FactorialMain();
-        System.out.println(main.calc(4));
-        System.out.println(main.calc(5));
-    }
-
     private int calc(int n) {
         int f = 1;
         for (int i = 2; i <= n; i++) {
             f *= i;
         }
         return f;
+    }
+
+    private int[] cache = new int[0];
+
+    private void ensureCacheSize(int n) {
+        if (cache.length >= n + 1) {
+            return;
+        }
+        int[] tmp = new int[n + 1];
+        System.arraycopy(cache, 0, tmp, 0, cache.length);
+        cache = tmp;
+    }
+
+    public int calcCached(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        ensureCacheSize(n);
+        int cached = cache[n];
+        if (cached != 0) {
+            return cached;
+        }
+        int val = calcCached(n - 1) * n;
+        cache[n] = val;
+        return val;
+    }
+
+    public static void main(String[] args) {
+        FactorialMain main = new FactorialMain();
+        System.out.println(main.calc(4));
+        System.out.println(main.calc(5));
+
+        System.out.println(main.calcCached(4));
+        System.out.println(main.calcCached(5));
     }
 }
